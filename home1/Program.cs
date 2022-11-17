b8acce751054ab05b5402a44828c1d990b64e800
Alex <gmail@alex.com>
2022-11-17 10:20:37 +0300

    task1 solved

--- home1/Program.cs
+++ home1/Program.cs
@@ -3,3 +3,15 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+
+Console.WriteLine ("Enter two numbers:");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine ($"Number {num1} is greater than {num2}");
+    }
+else
+{
+    Console.WriteLine ($"Number {num2} is greater than {num1}");
+}
