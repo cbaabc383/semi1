96f9725684edf8a0393225029e941502ef2d57d3
Alex <gmail@alex.com>
2022-11-18 17:52:50 +0300

    Second task

--- seminar2/home/Program.cs
+++ seminar2/home/Program.cs
@@ -4,15 +4,38 @@
 918 -> 1
 */
 
+/*
 int second (int num)
 {
     int dec = (num % 100 - num % 10) / 10;
     return dec;
 }
 
-Console.Write ("Input your 3digit number: ");
-
+Console.Write ("Input your 3-digit number: ");
 int UserNum = Convert.ToInt32 (Console.ReadLine());
+
 Console.Write ($"The second digit is {second(UserNum)}");
+*/
+
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int third (int num)
+{
+    int three = (num / 100) % 10;
+    return three;
+}
+Console.Write ("Input your number: ");
+int UserNum = Convert.ToInt32 (Console.ReadLine());
+if (UserNum > 99)
+    Console.Write ($"The third digit of your number is {third (UserNum)}");
+else
+    Console.Write ("Your number doesn't have third digit");
+
 
 
