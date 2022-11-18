ae032140b1619661b7762b1e22f9b462194ffbc6
Alex <gmail@alex.com>
2022-11-18 16:52:29 +0300

    Fist task

--- seminar2/home/Program.cs
+++ seminar2/home/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int second (int num)
+{
+    int dec = (num % 100 - num % 10) / 10;
+    return dec;
+}
+
+Console.Write ("Input your 3digit number: ");
+
+int UserNum = Convert.ToInt32 (Console.ReadLine());
+Console.Write ($"The second digit is {second(UserNum)}");
+
+
