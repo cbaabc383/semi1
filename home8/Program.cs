f3e5cc75716e470fdad3d8aebb87659f089dd3d3
Alex <gmail@alex.com>
2022-12-10 11:35:07 +0300

    he8

--- home8/Program.cs
+++ home8/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+/*
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+/*
+int[,] Create2Array(int row, int column, int minVal, int maxVal)
+{
+    int[,] createArray = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            createArray[i, j] = new Random().Next(minVal, maxVal);
+        }
+    }
+    return createArray;
+}
+
+void Show2Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] RowBackwards(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxRow = array[i, j];
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > maxRow)
+                maxRow = array[i, k];
+            }
+            temp = array[i, j];
+            array[i, j] = maxRow;
+        }
+        
+
+
+
+    }
+}
+
+*/
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+/*
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+
